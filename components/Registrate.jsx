6774354598cf764deaa788554cba7246481d5b36
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2024-04-26 01:14:01 -0600

    Update Registrate.jsx
    se cambio la url por una almoadilla

--- components/Registrate.jsx
+++ components/Registrate.jsx
@@ -4,7 +4,7 @@ const Registrate = () => {
     <section className="contenedor py-10">
          <h2 className="text-center text-h-gray-1 text-2xl mb-10 font-bold">¡Regístrate gratis!</h2>
           <Link 
-            href='/registro'
+            href='#'
             className="bg-h-green-1 block h-20 py-6 font-bold text-2xl text-center no-underline rounded"
           >Registrarme</Link>
     </section>

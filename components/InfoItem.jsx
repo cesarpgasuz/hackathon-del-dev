0fe39928a0ea1a7baad19abc6d8bce0d62bee4f0
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2024-05-11 15:40:30 -0600

    Update InfoItem.jsx
    se arreglo la alineación de la sección en que consiste

--- components/InfoItem.jsx
+++ components/InfoItem.jsx
@@ -1,6 +1,6 @@
 const InfoItem = ({children}) => {
   return (
-    <article className="flex gap-6 items-center text-h-gray-2 text-sm leading-7 md:flex-col md:justify-center md:text-center">{children}</article>
+    <article className="flex gap-6 items-center text-h-gray-2 text-sm leading-7 md:flex-col md:text-center">{children}</article>
   )
 }
 export default InfoItem
